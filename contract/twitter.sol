8a9ee4dc926200012d3b453a66800d610504c113
yuxiaobo123123 <461837619@qq.com>
2024-08-05 23:04:39 +0800

    CHANGE

--- contract/twitter.sol
+++ contract/twitter.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.0;
 
 contract Twitter{
 
+    uint16 public MAX_TWEET_LENGTH = 255;
+
     //defined struct
     struct Tweet{
         address author;
@@ -15,8 +17,26 @@ contract Twitter{
 
 
     mapping (address => Tweet[]) public tweets;
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner,"YOU ARE NOT THE OWNER");
+        _;
+    }
+
+
+    function changeTweetLength(uint16 newTweetLength) public onlyOwner{
+        MAX_TWEET_LENGTH = newTweetLength;
+    }
 
     function createTweet(string memory _tweet) public {
+
+        require(bytes(_tweet).length<=MAX_TWEET_LENGTH,"Your tweet is too long bro");
+
         Tweet memory newTweet = Tweet({
                 author: msg.sender,
                 content:_tweet,
@@ -26,8 +46,8 @@ contract Twitter{
         tweets[msg.sender].push(newTweet);
     }
 
-    function getTweet(address _owner,uint _i) public view returns (Tweet memory) {
-        return tweets[_owner][_i];
+    function getTweet(uint _i) public view returns (Tweet memory) {
+        return tweets[msg.sender][_i];
     }
 
     function getAllTweets(address _owner) public view returns(Tweet[] memory){
