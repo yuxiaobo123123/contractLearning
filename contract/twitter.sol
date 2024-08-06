35a89b5fd78209f274b0d7ed268159a635d766e9
Lalleluia <461837619@qq.com>
2024-08-06 17:15:41 +0800

    Twiteer sol complete

--- contract/twitter.sol
+++ contract/twitter.sol
@@ -9,6 +9,7 @@ contract Twitter{
 
     //defined struct
     struct Tweet{
+        uint256 id;
         address author;
         string content;
         uint256 timestamp;
@@ -18,6 +19,10 @@ contract Twitter{
 
     mapping (address => Tweet[]) public tweets;
     address public owner;
+
+    event TweetCreated(uint256 id, address auther, string content, uint256 timestamp);
+    event TweetLiked(address liker,address tweetAuthor,uint256 tweetId,uint newLikeCount);
+    event TweetUnliked(address unliker,address tweetAuthor,uint256 tweetId,uint newLikeCount);
 
     constructor(){
         owner = msg.sender;
@@ -38,12 +43,28 @@ contract Twitter{
         require(bytes(_tweet).length<=MAX_TWEET_LENGTH,"Your tweet is too long bro");
 
         Tweet memory newTweet = Tweet({
+                id:tweets[msg.sender].length,
                 author: msg.sender,
                 content:_tweet,
                 timestamp:block.timestamp,
                 likes:0
         });
         tweets[msg.sender].push(newTweet);
+
+        emit TweetCreated(newTweet.id,newTweet.author,newTweet.content,newTweet.timestamp);
+    }
+
+    function likeTweet(address author,uint256 id) external {
+        require(tweets[author][id].id==id,"TWEET DOSE NOT EXIST");
+        tweets[author][id].likes++;
+        emit TweetLiked(msg.sender,author,id,tweets[author][id].likes);
+    }
+    function unLikeTweet(address author,uint256 id) external  {
+        require(tweets[author][id].id==id,"TWEET DOSE NOT EXIST");
+        require(tweets[author][id].likes>0,"TWEET HAS NO LIKES");
+        tweets[author][id].likes--;
+
+        emit TweetUnliked(msg.sender,author,id,tweets[author][id].likes);
     }
 
     function getTweet(uint _i) public view returns (Tweet memory) {
